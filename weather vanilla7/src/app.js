43f041dfda1c35654fded5bc0dab52e85b1ae005
Fernanda <perezpenna@gmail.com>
2021-05-21 16:55:29 -0300

    added the search engine

--- weather vanilla7/src/app.js
+++ weather vanilla7/src/app.js
@@ -32,8 +32,19 @@ function displayTemperature(response) {
     iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "735e10c045040356eb9af5827a8bd827";
-let city = "São Paulo";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+    let apiKey = "735e10c045040356eb9af5827a8bd827";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
+
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
